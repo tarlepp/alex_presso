c80c94776fc3a9a11377d35a2d21ff1414839204
Tarmo Leppänen <tarmo.leppanen@protacon.com>
2019-03-12 19:36:44 +0200

    And actuallogic for serialization groups

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Foo;
+use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -18,11 +20,19 @@ class DefaultController extends AbstractController
      */
     public function index(SerializerInterface $serializer): JsonResponse
     {
-        $data = [
-            ['id' => 1, 'foo' => 'bar1', 'bar' => 'foo1'],
-            ['id' => 2, 'foo' => 'bar2', 'bar' => 'foo2'],
-        ];
+        $data = array_map(
+            function ($suffix) {
+                return (new Foo())
+                    ->setFoo('foo - ' . $suffix)
+                    ->setBar('bar - ' . $suffix);
+            },
+            range(1,3)
+        );
 
-        return new JsonResponse($serializer->serialize($data, 'json'), 200, [], true);
+        $context1 = (new SerializationContext())->setGroups('Foo');
+        $context2 = (new SerializationContext())->setGroups('Foo.foo');
+        $context3 = (new SerializationContext())->setGroups('Foo.bar');
+
+        return new JsonResponse($serializer->serialize($data, 'json', $context1), 200, [], true);
     }
 }
